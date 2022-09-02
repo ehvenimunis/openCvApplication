71b7f53eddecec9dbe613357681fb9295b74a2cf
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-09-02 20:42:06 +0300

    Drawing point on screen

--- openCvApp.cpp
+++ openCvApp.cpp
@@ -1,66 +1,88 @@
 ﻿/*
     Author: Muhammed Salih Aydoğan
-    Date:  31/08/2022
+    Date:  02/08/2022
 */
 #include <iostream>                        
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 using namespace std;
+using namespace cv;
 #define w 400
 #define size w/w
-using namespace cv;
 
-class Mouse {
+class App
+{
 public:
+    App();
+    ~App();
+    void setWin(const std::string& _winname);
+private:
     Mat image;
-    char window[];
-    Mouse() {
-        Mat image = Mat::zeros(w, w, CV_8UC3);
-        char window[] = "Screen For Mouse Handle";
-    }
-    void MyFilledCircle(int x, int y)
-    {
-        circle(image,
-            Point(x, y),
-            size,
-            Scalar(0, 0, 255),
-            FILLED,
-            LINE_8);
-        imshow(window, image);
-    }
-    void locator(int event, int x, int y, int flags, void* userdata) { //function to track mouse movement and click//
-        if (event == EVENT_LBUTTONDOWN) { //when left button clicked//
-            MyFilledCircle(x, y);
-            cout << "Left click has been made, Position:(" << x << "," << y << ")" << endl;
-        }
-        else if (event == EVENT_RBUTTONDOWN) { //when right button clicked//
-            MyFilledCircle(x, y);
-            cout << "Rightclick has been made, Position:(" << x << "," << y << ")" << endl;
-        }
-        else if (event == EVENT_MBUTTONDOWN) { //when middle button clicked//
-            MyFilledCircle(x, y);
-            cout << "Middleclick has been made, Position:(" << x << "," << y << ")" << endl;
-        }
-        else if (event == EVENT_MOUSEMOVE) { //when mouse pointer moves//
-            MyFilledCircle(x, y);
-            cout << "Current mouse position:(" << x << "," << y << ")" << endl;
-        }
-    }
+    std::string winname;
+    void on_mouse_internal(int ev, int x, int y);
+    void MyFilledCircle(int x, int y);
+    friend void on_mouse(int ev, int x, int y, int, void* obj);
 };
-int main(void) {
-    Mouse mt;
-    namedWindow(mt.window);//declaring window to show image//
-    setMouseCallback(mt.window, mt.locator, NULL);//Mouse callback function on define window
-    imshow(mt.window, mt.image);
-    waitKey(0);
-    /*
-    if (image.empty())
-    {
-        std::cout << "Could not read the image: " << std::endl;
-        return 1;
-    }
-    */
-    return(0);
+void on_mouse(int ev, int x, int y, int, void* obj)
+{
+    App* app = static_cast<App*>(obj);
+    if (app)
+        app->on_mouse_internal(ev, x, y);
 }
 
+App::App()
+{
+    
+}
+
+App::~App()
+{
+    
+}
+
+void App::setWin(const std::string& _winname)
+{
+    cv::namedWindow(_winname);
+    this->winname = _winname;
+    this->image = Mat::zeros(w, w, CV_8UC3);
+    cv::setMouseCallback(winname, on_mouse, this);
+    imshow(winname, image);
+    waitKey(0);
+}
+void App::MyFilledCircle(int x, int y)
+{
+    circle(image,
+        Point(x, y),
+        size,
+        Scalar(x, y, x+y),
+        FILLED,
+        LINE_8);
+    imshow(winname, image);
+}
+
+void App::on_mouse_internal(int ev, int x, int y)
+{
+    if (ev == EVENT_LBUTTONDOWN) { //when left button clicked//
+        MyFilledCircle(x,y);
+        cout << "Left click has been made, Position:(" << x << "," << y << ")" << endl;
+    }
+    else if (ev == EVENT_RBUTTONDOWN) { //when right button clicked//
+        MyFilledCircle(x, y);
+        cout << "Rightclick has been made, Position:(" << x << "," << y << ")" << endl;
+    }
+    else if (ev == EVENT_MBUTTONDOWN) { //when middle button clicked//
+        MyFilledCircle(x, y);
+        cout << "Middleclick has been made, Position:(" << x << "," << y << ")" << endl;
+    }
+    else if (ev == EVENT_MOUSEMOVE) { //when mouse pointer moves//
+        MyFilledCircle(x, y);
+        cout << "Current mouse position:(" << x << "," << y << ")" << endl;
+    }
+}
+
+int main(void) {
+    App mt;
+    mt.setWin("Screen For Mouse Handle");
+    return(0);
+}
