37ce0e4f91a6855a62821e29bcdf67e02fd04811
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-09-02 06:48:45 +0300

    Added filled circle method

--- openCvApp.cpp
+++ openCvApp.cpp
@@ -10,133 +10,51 @@
 using namespace std;
 #define w 400
 using namespace cv;
-/*
-void MyEllipse(Mat img, double angle);
-void MyFilledCircle(Mat img, Point center);
-void MyPolygon(Mat img);
-void MyLine(Mat img, Point start, Point end);
-*/
+
+Mat image = Mat::zeros(w, w, CV_8UC3);
+char window[] = "Screen For Mouse Handle";
+
+void MyFilledCircle(int x, int y)
+{
+    circle(image,
+        Point(x, y),
+        1 / w ,
+        Scalar(0, 0, 255),
+        FILLED,
+        LINE_8);
+    imshow(window, image);
+}
+
 void locator(int event, int x, int y, int flags, void* userdata) { //function to track mouse movement and click//
     if (event == EVENT_LBUTTONDOWN) { //when left button clicked//
         cout << "Left click has been made, Position:(" << x << "," << y << ")" << endl;
+        MyFilledCircle(x,y);
     }
     else if (event == EVENT_RBUTTONDOWN) { //when right button clicked//
+        MyFilledCircle(x,y);
         cout << "Rightclick has been made, Position:(" << x << "," << y << ")" << endl;
     }
     else if (event == EVENT_MBUTTONDOWN) { //when middle button clicked//
+        MyFilledCircle(x,y);
         cout << "Middleclick has been made, Position:(" << x << "," << y << ")" << endl;
     }
     else if (event == EVENT_MOUSEMOVE) { //when mouse pointer moves//
         cout << "Current mouse position:(" << x << "," << y << ")" << endl;
-    }
+        MyFilledCircle(x,y);
+    }    
 }
 int main(void) {
-    char window[] = "Drawing 1";
-    Mat image = Mat::zeros(w, w, CV_8UC3);
-
-    Mat img(w, w, CV_8UC3);
-    randu(img, Scalar(0, 0, 0), Scalar(255, 255, 255));
-    imshow("random colors image", img);
-
-    /*
-    MyEllipse(atom_image, 90);
-    MyEllipse(atom_image, 0);
-    MyEllipse(atom_image, 45);
-    MyEllipse(atom_image, -45);
-    MyFilledCircle(atom_image, Point(w / 2, w / 2));
-    MyPolygon(rook_image);
-    rectangle(rook_image,
-        Point(0, 7 * w / 8),
-        Point(w, w),
-        Scalar(0, 255, 255),
-        FILLED,
-        LINE_8);
-    MyLine(rook_image, Point(0, 15 * w / 16), Point(w, 15 * w / 16));
-    MyLine(rook_image, Point(w / 4, 7 * w / 8), Point(w / 4, w));
-    MyLine(rook_image, Point(w / 2, 7 * w / 8), Point(w / 2, w));
-    MyLine(rook_image, Point(3 * w / 4, 7 * w / 8), Point(3 * w / 4, w));
-    */
 
     if (image.empty())
     {
         std::cout << "Could not read the image: " << std::endl;
         return 1;
     }
-    namedWindow("Drawing 1");//declaring window to show image//
-    setMouseCallback("Drawing 1", locator, NULL);//Mouse callback function on define window//
+
+    namedWindow(window);//declaring window to show image//
+    setMouseCallback(window, locator, NULL);//Mouse callback function on define window
     imshow(window, image);
-    moveWindow(window, w, 200);
     waitKey(0);
     return(0);
 }
 
-
-/*
-void MyEllipse(Mat img, double angle)
-{
-    int thickness = 2;
-    int lineType = 8;
-    ellipse(img,
-        Point(w / 2, w / 2),
-        Size(w / 4, w / 16),
-        angle,
-        0,
-        360,
-        Scalar(255, 0, 0),
-        thickness,
-        lineType);
-}
-void MyFilledCircle(Mat img, Point center)
-{
-    circle(img,
-        center,
-        w / 32,
-        Scalar(0, 0, 255),
-        FILLED,
-        LINE_8);
-}
-void MyPolygon(Mat img)
-{
-    int lineType = LINE_8;
-    Point rook_points[1][20];
-    rook_points[0][0] = Point(w / 4, 7 * w / 8);
-    rook_points[0][1] = Point(3 * w / 4, 7 * w / 8);
-    rook_points[0][2] = Point(3 * w / 4, 13 * w / 16);
-    rook_points[0][3] = Point(11 * w / 16, 13 * w / 16);
-    rook_points[0][4] = Point(19 * w / 32, 3 * w / 8);
-    rook_points[0][5] = Point(3 * w / 4, 3 * w / 8);
-    rook_points[0][6] = Point(3 * w / 4, w / 8);
-    rook_points[0][7] = Point(26 * w / 40, w / 8);
-    rook_points[0][8] = Point(26 * w / 40, w / 4);
-    rook_points[0][9] = Point(22 * w / 40, w / 4);
-    rook_points[0][10] = Point(22 * w / 40, w / 8);
-    rook_points[0][11] = Point(18 * w / 40, w / 8);
-    rook_points[0][12] = Point(18 * w / 40, w / 4);
-    rook_points[0][13] = Point(14 * w / 40, w / 4);
-    rook_points[0][14] = Point(14 * w / 40, w / 8);
-    rook_points[0][15] = Point(w / 4, w / 8);
-    rook_points[0][16] = Point(w / 4, 3 * w / 8);
-    rook_points[0][17] = Point(13 * w / 32, 3 * w / 8);
-    rook_points[0][18] = Point(5 * w / 16, 13 * w / 16);
-    rook_points[0][19] = Point(w / 4, 13 * w / 16);
-    const Point* ppt[1] = { rook_points[0] };
-    int npt[] = { 20 };
-    fillPoly(img,
-        ppt,
-        npt,
-        1,
-        Scalar(255, 255, 255),
-        lineType);
-}
-void MyLine(Mat img, Point start, Point end)
-{
-    int thickness = 2;
-    int lineType = LINE_8;
-    line(img,
-        start,
-        end,
-        Scalar(0, 0, 0),
-        thickness,
-        lineType);
-}
-*/
